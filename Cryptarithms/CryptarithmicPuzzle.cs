147ead15c5091b22a1c4bf320c480a061a0c070f
John Fox <J.Arcoudis@gmail.com>
2018-03-01 20:26:13 +1100

    Add basic Letter(variable) extraction

---
+++ Cryptarithms/CryptarithmicPuzzle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptarithms
+{
+    class CryptarithmicPuzzle
+    {
+        // Array containing arithmic question.
+        // Array of Letter Variables to adjust.
+        // String of original input.
+        // 
+
+        private string puzzleString;
+        private List<char> puzzleLetters;
+        private string puzzleEquation;
+
+        public struct LetterValPair
+        {
+            // TODO: Getter and Setters
+            public char letter;
+            public int value;
+        }
+
+        public CryptarithmicPuzzle(string Puzzle)
+        {
+            puzzleString = Puzzle;
+            puzzleLetters = extractVariables(Puzzle);
+        }
+
+        private List<char> extractVariables(string puzzleString)
+        {
+
+            List<char> variables = new List<char>();
+            for (int i = 0; i < puzzleString.Length; i++)
+            {
+                // Get char and check if valid Letter
+                char c = puzzleString[i];
+                if (!Char.IsLetter(c)) continue;
+
+                bool exists = false;
+                // Check to see if letter is already recorded
+                foreach (char item in variables)
+                {
+                    // If Exists, Skip loop and continue
+                    if (item.Equals(c))
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists)
+                {
+                    variables.Add(c);
+
+                }
+            }
+
+            return variables; 
+        }
+
+    }
+}
---
+++ Cryptarithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptarithms
+{
+    class Program
+    {
+        // Solution can't have leading Zeros's. (Left Most digit cant be Zero)
+        // All Unique Numbers
+        // Base 10 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.ToString());
+            CryptarithmicPuzzle puzzle = new CryptarithmicPuzzle(args[0]);
+        }
+    }
+}
